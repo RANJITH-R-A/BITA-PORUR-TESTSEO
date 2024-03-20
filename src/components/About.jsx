bd83dae76802419214f82ca633619653af0c205c
RANJITH-R-A <ranjithramasamy53@gmail.com>
2024-03-20 13:56:24 +0530

    changecanonicalmethod1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,18 @@
 import React, { Suspense } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useLocation } from 'react-router'
 
 
 const About = () => {
+  const urllocation = useLocation();
+  const canonicalUrl = "https://lighthearted-cajeta-4feab8.netlify.app" + urllocation.pathname;
   return (
     <>
      <Helmet>
       <title>about</title>
       <meta name="description" content="about page need page information" />
-      <link rel="canonical" href="/about" />
+      <link rel="canonical" href={canonicalUrl} />
+      {/* <link rel="canonical" href="/about" /> */}
     </Helmet>
       <section>
         <h1>About Page</h1>
--- src/components/MyRoutes.jsx
+++ src/components/MyRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../components/Home'
 import Header from '../components/Header'
 import About from '../components/About'
@@ -12,17 +12,17 @@ import Footer from './Footer'
 const MyRoutes = () => {
   return (
     <>
-    <BrowserRouter>
-   <Header/>
-    <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/myteam" element={<MyTeam/>} />
-        <Route path="/contact" element={<Contact/>} />
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="myteam" element={<MyTeam />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
