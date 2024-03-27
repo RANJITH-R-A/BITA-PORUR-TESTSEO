8e8a2aa6ff88c28a6be768d6edf3096e01bc70b9
RANJITH-R-A <ranjithramasamy53@gmail.com>
2024-03-27 11:53:03 +0530

    add og meta tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router';
+import homeimg from '../../public/all-images/md_bgimg01.jpeg'
 
 const Home = () => {
   const urllocation1 = useLocation();
@@ -11,6 +12,10 @@ const Home = () => {
         <title>home</title>
       <meta name="description" content="home page need page information" />
         <link rel="canonical" href={canonicalUrl1} />
+        <meta property="og:title" content="Light Cluod" />
+        <meta property="og:description" content="home page need page information." />
+        <meta property="og:image" content={canonicalUrl1+homeimg} />
+        <meta property="og:url" content={canonicalUrl1} />
       </Helmet>
       <section >
         <h1>Home Page</h1>
